6763203f93c2c0d3d2ba9c5858d4f4021b8f1255
p1anktongg <plankton84@bk.ru>
2025-06-25 13:56:29 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,14 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
+  .helpOption('-h, --help', 'display help for command')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to first file')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+    console.log(`Output format: ${program.opts().format}`);
+  });
 
-program.parse();
+program.parse(process.args);
