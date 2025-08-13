f666328d97837a574d32aca5ad97b378ba3e6cef
p1anktongg <plankton84@bk.ru>
2025-08-13 17:34:49 +0300

    added asciinema

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/p1anktongg/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/p1anktongg/frontend-project-46/actions)
+
+### Asciinema of gendiff work
+[![asciicast](https://asciinema.org/a/MQhMOa4r5y5lYwx9JrnSvJrHx.svg)](https://asciinema.org/a/MQhMOa4r5y5lYwx9JrnSvJrHx) 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+import _ from 'lodash';
 import parseFile from './parsers.js';
+
+const buildDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = _.sortBy(_.union(keys1, keys2));
+
+  const lines = allKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return [`  - ${key}: ${data1[key]}`, `  + ${key}: ${data2[key]}`].join('\n');
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return ['{', ...lines, '}'].join('\n');
+};
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   try {
     const data1 = parseFile(filepath1);
     const data2 = parseFile(filepath2);
     
-    // Пока просто возвращаем информацию о прочитанных данных
-    return `Successfully read files:\nFile 1: ${JSON.stringify(data1)}\nFile 2: ${JSON.stringify(data2)}`;
+    if (format === 'stylish') {
+      return buildDiff(data1, data2);
+    }
+    
+    // Пока поддерживаем только stylish формат
+    return buildDiff(data1, data2);
   } catch (error) {
     return `Error: ${error.message}`;
   }
