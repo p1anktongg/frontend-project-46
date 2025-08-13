cf28cf217b45e0e4f72e2076ca60e4201d2c1974
p1anktongg <plankton84@bk.ru>
2025-08-13 16:35:27 +0300

    rework project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  // Пока пустая функция, реализуем позже
+  // format - параметр формата вывода
+  return `Comparing ${filepath1} and ${filepath2} in ${format} format`;
+};
+
+export default genDiff;
